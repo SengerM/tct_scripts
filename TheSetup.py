cb394c52fa4ef45ea17486d2a5070705b53c589c
Matias Senger <m.senger@hotmail.com>
2021-10-06 01:59:31 +0200

    First working prototype controlling everything

---
+++ TheSetup.py
@@ -0,0 +1,77 @@
+import PyticularsTCT # https://github.com/SengerM/PyticularsTCT
+import pyvisa
+import TeledyneLeCroyPy # https://github.com/SengerM/TeledyneLeCroyPy
+from keithley.Keithley2470 import Keithley2470SafeForLGADs
+
+class TheSetup:
+	"""This class wraps all the hardware so if there are changes it is easy to adapt."""
+	def __init__(self):
+		self._osc = TeledyneLeCroyPy.LeCroyWaveRunner(pyvisa.ResourceManager().open_resource('USB0::0x05FF::0x1023::4751N40408::INSTR'))
+		self._tct = PyticularsTCT.ParticularsTCT()
+		self._keithley = Keithley2470SafeForLGADs('USB0::1510::9328::04481179::0::INSTR', polarity = 'negative')
+		
+		self._keithley.set_output('on')
+	
+	def move_to(self, x=None, y=None, z=None):
+		"""Move the TCT stages to the specified position."""
+		self._tct.stages.move_to(x=x,y=y,z=z)
+	
+	@property
+	def position(self):
+		"""Returns the position of the stages."""
+		return self._tct.stages.position
+	
+	@property
+	def laser_status(self):
+		"""Return the laser status "on" or "off"."""
+		return self._tct.laser.status
+	@laser_status.setter
+	def laser_status(self, status):
+		"""Set the laser status "on" or "off"."""
+		self._tct.laser.status = status
+	
+	@property
+	def laser_DAC(self):
+		"""Returns the laser DAC value."""
+		return self._tct.laser.DAC
+	@laser_DAC.setter
+	def laser_DAC(self, value):
+		"""Set the value of the DAC for the laser."""
+		self._tct.laser.DAC = value
+	
+	def wait_for_trigger(self):
+		"""Blocks execution until there is a trigger in the oscilloscope."""
+		self._osc.wait_for_single_trigger()
+	
+	def get_waveform(self, channel: int):
+		"""Gets the waveform from the oscilloscope for the respective channel."""
+		return self._osc.get_waveform(channel=channel)
+	
+	@property
+	def bias_voltage(self):
+		"""Returns the measured bias voltage."""
+		return self._keithley.measure_voltage()
+	@bias_voltage.setter
+	def bias_voltage(self, volts):
+		"""Sets the bias voltage."""
+		self._keithley.set_source_voltage(volts)
+	
+	@property
+	def bias_current(self):
+		"""Returns the measured bias current."""
+		return self._keithley.measure_current()
+	
+	def current_compliance(self, amperes):
+		"""Sets the current compliance."""
+		self._keithley.current_limit = amperes
+
+if __name__ == '__main__':
+	import time
+	
+	the_setup = TheSetup()
+	
+	print(f'Bias voltage = {the_setup.bias_voltage} V')
+	for v in [11,22,11]:
+		the_setup.bias_voltage = v
+		print(f'Bias voltage = {the_setup.bias_voltage} V')
+		time.sleep(1)
--- linear_scan_many_triggers_per_point.py
+++ linear_scan_many_triggers_per_point.py
@@ -1,20 +1,19 @@
-import PyticularsTCT # https://github.com/SengerM/PyticularsTCT
-import pyvisa
-import TeledyneLeCroyPy # https://github.com/SengerM/TeledyneLeCroyPy
 import numpy as np
 from time import sleep
-import myplotlib as mpl
+import grafica # https://github.com/SengerM/grafica
 from lgadtools.LGADSignal import LGADSignal # https://github.com/SengerM/lgadtools
 from data_processing_bureaucrat.Bureaucrat import Bureaucrat, TelegramReportingInformation # https://github.com/SengerM/data_processing_bureaucrat
 from progressreporting.TelegramProgressReporter import TelegramReporter # https://github.com/SengerM/progressreporting
 from pathlib import Path
 from plotting_scripts.plot_everything_from_linear_scan import script_core as plot_everything_from_linear_scan
+from TheSetup import TheSetup
 
-CHANNELS = ['CH1', 'CH2', 'CH3', 'CH4']
 TIMES_AT = [10,20,30,40,50,60,70,80,90]
 
 def script_core(
 	measurement_name: str, 
+	bias_voltage: float,
+	laser_DAC: float,
 	x_start: float, 
 	x_end: float, 
 	y_start: float, 
@@ -22,6 +21,7 @@ def script_core(
 	z_focus: float, 
 	n_steps: int, 
 	n_triggers: int = 1,
+	acquire_channels = [1,2,3,4],
 ):
 	for var in [x_start, x_end, y_start, y_end, z_focus]:
 		if var**2 > 1: 
@@ -32,24 +32,24 @@ def script_core(
 		new_measurement = True,
 	)
 	
-	osc = TeledyneLeCroyPy.LeCroyWaveRunner(pyvisa.ResourceManager().open_resource('USB0::0x05FF::0x1023::4751N40408::INSTR'))
-	print(f'Connected with oscilloscope: {osc.idn}')
+	the_setup = TheSetup()
 	
-	tct = PyticularsTCT.ParticularsTCT()
+	the_setup.laser_DAC = laser_DAC
+	the_setup.laser_status = 'on'
 	
-	tct.laser.DAC = 0
-	tct.laser.on()
-
 	print('Moving to start position...')
-	tct.stages.move_to(
+	the_setup.move_to(
 		x = x_start,
 		y = y_start,
 		z = z_focus,
 	)
 	
+	print('Setting bias voltage...')
+	the_setup.bias_voltage = bias_voltage
+	
 	ofile_path = bureaucrat.processed_data_dir_path/Path('measured_data.csv')
 	with open(ofile_path, 'w') as ofile:
-		string = f'n_pos\tn_trigger\tx (m)\ty (m)\tz (m)\tn_channel\tAmplitude (V)\tNoise (V)\tRise time (s)\tCollected charge (a.u.)\tTime over noise (s)'
+		string = f'n_position\tn_trigger\tx (m)\ty (m)\tz (m)\tn_channel\tAmplitude (V)\tNoise (V)\tRise time (s)\tCollected charge (V s)\tTime over noise (s)'
 		for pp in TIMES_AT:
 			string += f'\tt_{pp} (s)'
 		print(string, file = ofile)
@@ -63,45 +63,45 @@ def script_core(
 	with reporter.report_for_loop(n_steps*n_triggers, f'{bureaucrat.measurement_name}') as reporter:
 		with open(ofile_path, 'a') as ofile:
 			for n_pos,xy_position in enumerate([(x,y) for x,y in zip(x_positions,y_positions)]):
-				tct.stages.move_to(
+				the_setup.move_to(
 					x = xy_position[0],
 					y = xy_position[1],
 				)
 				sleep(0.1)
-				position = tct.stages.position
+				position = the_setup.position
 				for n in range(n_triggers):
-					print(f'Preparing to acquire signals at n_pos={n_pos}, n_trigger={n}...')
-					osc.wait_for_single_trigger()
+					print(f'Measuring: n_position={n_pos}, n_trigger={n}...')
+					the_setup.wait_for_trigger()
 					signals = {}
-					for idx,ch in enumerate(CHANNELS):
-						raw_data = osc.get_waveform(channel = int(ch[-1]))
-						signals[ch] = LGADSignal(
+					for n_ch in acquire_channels:
+						raw_data = the_setup.get_waveform(channel = n_ch)
+						signals[n_ch] = LGADSignal(
 							time = raw_data['Time (s)'],
 							samples = raw_data['Amplitude (V)'],
 						)
-						string = f'{n_pos}\t{n}\t{position[0]:.6e}\t{position[1]:.6e}\t{position[2]:.6e}\t{idx+1}'
-						string += f'\t{signals[ch].amplitude:.6e}\t{signals[ch].noise:.6e}\t{signals[ch].rise_time:.6e}\t{signals[ch].collected_charge:.6e}\t{signals[ch].time_over_noise:.6e}'
+						string = f'{n_pos}\t{n}\t{position[0]:.6e}\t{position[1]:.6e}\t{position[2]:.6e}\t{n_ch}'
+						string += f'\t{signals[n_ch].amplitude:.6e}\t{signals[n_ch].noise:.6e}\t{signals[n_ch].rise_time:.6e}\t{signals[n_ch].collected_charge:.6e}\t{signals[n_ch].time_over_noise:.6e}'
 						for pp in TIMES_AT:
 							try:
-								string += f'\t{signals[ch].find_time_at_rising_edge(pp):.6e}'
+								string += f'\t{signals[n_ch].find_time_at_rising_edge(pp):.6e}'
 							except:
 								string += f'\t{float("NaN")}'
 						print(string, file = ofile)
 					if np.random.rand() < 20/n_steps/n_triggers:
-						for idx,ch in enumerate(CHANNELS):
-							fig = mpl.manager.new(
-								title = f'Signal at {n_pos:05d} n_trigg {n} {ch}',
+						for n_ch in acquire_channels:
+							fig = grafica.new(
+								title = f'Signal at {n_pos:05d} n_trigg {n} n_ch {n_ch}',
 								subtitle = f'Measurement: {bureaucrat.measurement_name}',
 								xlabel = 'Time (s)',
 								ylabel = 'Amplitude (V)',
-								package = 'plotly',
+								plotter_name = 'plotly',
 							)
-							signals[ch].plot_myplotlib(fig)
+							signals[n_ch].plot_grafica(fig)
 							for pp in TIMES_AT:
 								try:
 									fig.plot(
-										[signals[ch].find_time_at_rising_edge(pp)],
-										[signals[ch].signal_at(signals[ch].find_time_at_rising_edge(pp))],
+										[signals[n_ch].find_time_at_rising_edge(pp)],
+										[signals[n_ch].signal_at(signals[n_ch].find_time_at_rising_edge(pp))],
 										marker = 'x',
 										linestyle = '',
 										label = f'Time at {pp} %',
@@ -109,7 +109,7 @@ def script_core(
 									)
 								except:
 									pass
-							mpl.manager.save_all(mkdir=bureaucrat.processed_data_dir_path/Path('some_random_processed_signals_plots'))
+							grafica.save_unsaved(mkdir=bureaucrat.processed_data_dir_path/Path('some_random_processed_signals_plots'))
 					reporter.update(1)
 	print('Finished measuring! :)')
 	print('Doing plots...')
@@ -127,6 +127,8 @@ if __name__ == '__main__':
 	
 	script_core(
 		measurement_name = input('Measurement name? ').replace(' ', '_'),
+		bias_voltage = 99,
+		laser_DAC = 222,
 		x_start = X_FIXED,
 		x_end = X_FIXED,
 		y_start = Y_START,
@@ -134,5 +136,6 @@ if __name__ == '__main__':
 		n_steps = int(((Y_STOP-Y_START)**2)**.5/STEP_SIZE),
 		z_focus = 52.71626953125e-3,
 		n_triggers = 4,
+		acquire_channels = [1,2],
 	)
 
