b633764e6a81f6bd7325d623e3a5804673f3fcb2
Matias Senger <m.senger@hotmail.com>
2021-10-08 08:43:43 +0200

    Script for linear scan along any curve

--- linear_scan_many_triggers_per_point.py
+++ linear_scan_many_triggers_per_point.py
@@ -7,7 +7,6 @@ from progressreporting.TelegramProgressReporter import TelegramReporter # https:
 from pathlib import Path
 from plotting_scripts.plot_everything_from_linear_scan import script_core as plot_everything_from_linear_scan
 from TheSetup import TheSetup
-import pandas
 
 TIMES_AT = [10,20,30,40,50,60,70,80,90]
 
@@ -15,15 +14,10 @@ def script_core(
 		measurement_name: str, 
 		bias_voltage: float,
 		laser_DAC: float,
-		x_start: float, 
-		x_end: float, 
-		y_start: float, 
-		y_end: float, 
-		z_start: float,
-		z_end: float,
-		n_steps: int, 
+		positions: list, # This is a list of iterables with 3 floats, each element of the form (x,y,z).
 		n_triggers: int = 1,
 		acquire_channels = [1,2,3,4],
+		two_pulses = False,
 	):
 	bureaucrat = Bureaucrat(
 		str(Path(f'C:/Users/tct_cms/Desktop/TCT_measurements_data/{measurement_name}')),
@@ -37,79 +31,79 @@ def script_core(
 	the_setup.laser_DAC = laser_DAC
 	the_setup.laser_status = 'on'
 	
-	print('Moving to start position...')
-	the_setup.move_to(
-		x = x_start,
-		y = y_start,
-		z = z_start,
-	)
-	
 	print('Setting bias voltage...')
 	the_setup.bias_voltage = bias_voltage
 	
 	ofile_path = bureaucrat.processed_data_dir_path/Path('measured_data.csv')
 	with open(ofile_path, 'w') as ofile:
-		string = f'n_position,n_trigger,x (m),y (m),z (m),n_channel,Amplitude (V),Noise (V),Rise time (s),Collected charge (V s),Time over noise (s)'
+		string = f'n_position,n_trigger,x (m),y (m),z (m),n_channel,n_pulse,Amplitude (V),Noise (V),Rise time (s),Collected charge (V s),Time over noise (s)'
 		for pp in TIMES_AT:
 			string += f',t_{pp} (s)'
 		print(string, file = ofile)
 	
-	positions = pandas.DataFrame({
-		'x': list(np.linspace(x_start,x_end,n_steps)),
-		'y': list(np.linspace(y_start,y_end,n_steps)),
-		'z': list(np.linspace(z_start,z_end,n_steps)),
-	})
 	reporter = TelegramReporter(
 		telegram_token = TelegramReportingInformation().token, 
 		telegram_chat_id = TelegramReportingInformation().chat_id,
 	)
-	with reporter.report_for_loop(n_steps*n_triggers, f'{bureaucrat.measurement_name}') as reporter:
+	with reporter.report_for_loop(len(positions)*n_triggers, f'{bureaucrat.measurement_name}') as reporter:
 		with open(ofile_path, 'a') as ofile:
-			for (n_pos, move_to_position) in positions.iterrows():
-				the_setup.move_to(*move_to_position)
+			for n_position, target_position in enumerate(positions):
+				the_setup.move_to(*target_position)
 				sleep(0.1)
 				position = the_setup.position
-				for n in range(n_triggers):
-					print(f'Measuring: n_position={n_pos}, n_trigger={n}...')
+				for n_trigger in range(n_triggers):
+					plot_this_trigger = np.random.rand() < 20/(len(positions)*n_triggers)
+					print(f'Measuring: n_position={n_position}, n_trigger={n_trigger}...')
 					the_setup.wait_for_trigger()
 					signals = {}
 					for n_ch in acquire_channels:
-						raw_data = the_setup.get_waveform(channel = n_ch)
-						signals[n_ch] = LGADSignal(
-							time = raw_data['Time (s)'],
-							samples = raw_data['Amplitude (V)'],
-						)
-						string = f'{n_pos},{n},{position[0]:.6e},{position[1]:.6e},{position[2]:.6e},{n_ch}'
-						string += f',{signals[n_ch].amplitude:.6e},{signals[n_ch].noise:.6e},{signals[n_ch].rise_time:.6e},{signals[n_ch].collected_charge:.6e},{signals[n_ch].time_over_noise:.6e}'
-						for pp in TIMES_AT:
-							try:
-								string += f',{signals[n_ch].find_time_at_rising_edge(pp):.6e}'
-							except:
-								string += f',{float("NaN")}'
-						print(string, file = ofile)
-					if np.random.rand() < 20/n_steps/n_triggers:
-						for n_ch in acquire_channels:
-							fig = grafica.new(
-								title = f'Signal at {n_pos:05d} n_trigg {n} n_ch {n_ch}',
-								subtitle = f'Measurement: {bureaucrat.measurement_name}',
-								xlabel = 'Time (s)',
-								ylabel = 'Amplitude (V)',
-								plotter_name = 'plotly',
+						try:
+							raw_data = the_setup.get_waveform(channel = n_ch)
+						except Exception as e:
+							print(f'Cannot get data from oscilloscope, reason: {e}')
+						raw_data_each_pulse = {}
+						if not two_pulses:
+							raw_data_each_pulse[1] = raw_data
+						else:
+							for n_pulse in [1,2]:
+								raw_data_each_pulse[n_pulse] = {}
+								for variable in ['Time (s)','Amplitude (V)']:
+									if n_pulse == 1:
+										raw_data_each_pulse[n_pulse][variable] = raw_data[variable][:int(len(raw_data[variable])/2)]
+									if n_pulse == 2:
+										raw_data_each_pulse[n_pulse][variable] = raw_data[variable][int(len(raw_data[variable])/2):]
+						for n_pulse in raw_data_each_pulse.keys():
+							signal = LGADSignal(
+								time = raw_data_each_pulse[n_pulse]['Time (s)'],
+								samples = raw_data_each_pulse[n_pulse]['Amplitude (V)'],
 							)
-							signals[n_ch].plot_grafica(fig)
+							string = f'{n_position},{n_trigger},{position[0]},{position[1]},{position[2]},{n_ch},{n_pulse}'
+							string += f',{signal.amplitude},{signal.noise},{signal.rise_time},{signal.collected_charge},{signal.time_over_noise}'
 							for pp in TIMES_AT:
-								try:
-									fig.plot(
-										[signals[n_ch].find_time_at_rising_edge(pp)],
-										[signals[n_ch].signal_at(signals[n_ch].find_time_at_rising_edge(pp))],
-										marker = 'x',
-										linestyle = '',
-										label = f'Time at {pp} %',
-										color = (0,0,0),
-									)
-								except:
-									pass
-							grafica.save_unsaved(mkdir=bureaucrat.processed_data_dir_path/Path('some_random_processed_signals_plots'))
+								string += f',{signal.time_at_rising_edge(pp)}'
+							print(string, file = ofile)
+							if plot_this_trigger:
+								fig = grafica.new(
+									title = f'Signal at n_position {n_position} n_trigger {n_trigger} n_ch {n_ch} n_pulse {n_pulse}',
+									subtitle = f'Measurement: {bureaucrat.measurement_name}',
+									xlabel = 'Time (s)',
+									ylabel = 'Amplitude (V)',
+									plotter_name = 'plotly',
+								)
+								signal.plot_grafica(fig)
+								for pp in TIMES_AT:
+									try:
+										fig.scatter(
+											[signal.time_at_rising_edge(pp)],
+											[signal.signal_at(signal.time_at_rising_edge(pp))],
+											marker = 'x',
+											linestyle = 'none',
+											label = f'Time at {pp} %',
+											color = (0,0,0),
+										)
+									except Exception as e:
+										print(f'Cannot plot "times at X %", reason {e}.')
+								grafica.save_unsaved(mkdir=bureaucrat.processed_data_dir_path/Path('some_random_processed_signals_plots'))
 					reporter.update(1)
 	print('Finished measuring! :)')
 	print('Doing plots...')
@@ -119,26 +113,23 @@ def script_core(
 ########################################################################
 
 if __name__ == '__main__':
+	X_MIDDLE = 1.141396484375e-3
+	Y_MIDDLE = 9.58572265625e-3
+	Z_FOCUS = 51.901474609375e-3
+	STEP_SIZE = 1e-6
+	SWEEP_LENGTH = 250e-6
 	
-	X_START = 1.2335546874999999e-3 - 200e-6
-	X_STOP = X_START + 2*200e-6
-	Y_FIXED = 9.667763671874999e-3
-	STEP_SIZE = 9e-6
-	
-	Z_FOCUS = 53.34380859375e-3
+	x_positions = np.linspace(-SWEEP_LENGTH/2,SWEEP_LENGTH/2,int(SWEEP_LENGTH/STEP_SIZE)) + X_MIDDLE
+	y_positions = x_positions*0 + Y_MIDDLE
+	z_positions = x_positions*0 + Z_FOCUS
 	
 	script_core(
 		measurement_name = input('Measurement name? ').replace(' ', '_'),
-		bias_voltage = 99,
-		laser_DAC = 0,
-		x_start = X_START,
-		x_end = X_STOP,
-		y_start = Y_FIXED,
-		y_end = Y_FIXED,
-		z_start = Z_FOCUS,
-		z_end = Z_FOCUS,
-		n_steps = int(((X_STOP-X_START)**2)**.5/STEP_SIZE),
-		n_triggers = 4,
+		bias_voltage = 111,
+		laser_DAC = 1555, # Approximately should be 5 MIPs
+		positions = list(zip(x_positions,y_positions,z_positions)),
+		n_triggers = 555,
 		acquire_channels = [1,2],
+		two_pulses = True,
 	)
 
