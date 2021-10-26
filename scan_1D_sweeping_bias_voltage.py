8fde1e7b813a9e2f52277e5fd0c75eb8e088e3b5
Matias Senger <m.senger@hotmail.com>
2021-10-26 08:48:18 +0200

    Trying to fight against the memory leak problem

--- scan_1D_sweeping_bias_voltage.py
+++ scan_1D_sweeping_bias_voltage.py
@@ -8,12 +8,12 @@ from progressreporting.TelegramProgressReporter import TelegramReporter # https:
 import plotly.express as px
 import time
 
-N_TRIGGERS_PER_POSITION = 111
-BIAS_VOLTAGES = [11,22,33,44]
+N_TRIGGERS_PER_POSITION = 55
+BIAS_VOLTAGES = [222,199,177,155,144,122,99]
 X_MIDDLE = -1.478975e-3
 Y_MIDDLE = 9.21332e-3
 Z_FOCUS = .05219872
-STEP_SIZE = 1e-6
+STEP_SIZE = .5e-6
 SWEEP_LENGTH = 333e-6
 
 x_positions = X_MIDDLE + np.linspace(-SWEEP_LENGTH/2,SWEEP_LENGTH/2,int(SWEEP_LENGTH/STEP_SIZE))
@@ -39,16 +39,17 @@ reporter = TelegramReporter(
 with reporter.report_for_loop(len(x_positions)*N_TRIGGERS_PER_POSITION*len(BIAS_VOLTAGES), f'{bureaucrat.measurement_name}') as reporter:
 	with open(bureaucrat.processed_data_dir_path/Path(f'README.txt'),'w') as ofile:
 		print(f'This measurement created automatically all the following measurements:',file=ofile)
-		for bias_voltage in BIAS_VOLTAGES:
-			measurement_base_path = scan_1D(
-				measurement_name = f'{device_name}_1DScan_{bias_voltage}V',
-				the_setup = the_setup,
-				bias_voltage = bias_voltage,
-				laser_DAC = 2000,
-				positions = list(zip(x_positions,y_positions,z_positions)),
-				n_triggers = N_TRIGGERS_PER_POSITION,
-				acquire_channels = [1,2],
-				two_pulses = True,
-				external_Telegram_reporter = reporter,
-			)
+	for bias_voltage in BIAS_VOLTAGES:
+		measurement_base_path = scan_1D(
+			measurement_name = f'{device_name}_1DScan_{bias_voltage}V',
+			the_setup = the_setup,
+			bias_voltage = bias_voltage,
+			laser_DAC = 2000,
+			positions = list(zip(x_positions,y_positions,z_positions)),
+			n_triggers = N_TRIGGERS_PER_POSITION,
+			acquire_channels = [1,2],
+			two_pulses = True,
+			external_Telegram_reporter = reporter,
+		)
+		with open(bureaucrat.processed_data_dir_path/Path(f'README.txt'),'w') as ofile:
 			print(measurement_base_path.parts[-1],file=ofile)
