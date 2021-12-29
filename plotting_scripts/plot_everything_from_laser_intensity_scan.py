c814dfe7884d03030cc51eb0cd17239b425b0691
Matias Senger <m.senger@hotmail.com>
2021-12-29 11:24:48 +0100

    Major improvement

--- plotting_scripts/plot_everything_from_laser_intensity_scan.py
+++ plotting_scripts/plot_everything_from_laser_intensity_scan.py
@@ -1,10 +1,10 @@
 from data_processing_bureaucrat.Bureaucrat import Bureaucrat
 import numpy as np
 from pathlib import Path
-import myplotlib as mpl
+import plotly.express as px
 import pandas
-
-STATISTICAL_QUANTITIES = ['mean', 'std']
+from grafica.plotly_utils.utils import line
+from .plot_everything_from_1D_scan import mean_std
 
 def script_core(directory):
 	bureaucrat = Bureaucrat(
@@ -12,110 +12,63 @@ def script_core(directory):
 		variables = locals(),
 	)
 	
-	
-	data = pandas.read_csv(
-		bureaucrat.processed_by_script_dir_path('laser_intensity_scan.py')/Path('measured_data.csv'),
+	measured_data_df = pandas.read_feather(
+		bureaucrat.processed_by_script_dir_path('scan_laser_intensity.py')/Path('measured_data.fd'),
 	)
 	
-	n_DAC = sorted(set(data['n_DAC']))
+	GROUP_BY = ['n_DAC','n_channel','n_pulse','Laser DAC']
+	averaged_df = mean_std(df=measured_data_df, by=GROUP_BY)
 	
-	for column in data:
-		if column in ['n_DAC', 'n_trigger', 'x (m)', 'y (m)', 'z (m)', 'n_channel', 'n_pulse']:
+	mean_std_plots_dir_Path = bureaucrat.processed_data_dir_path/Path('mean_std_plots')
+	mean_std_plots_dir_Path.mkdir(parents=True, exist_ok=True)
+	
+	for column in averaged_df:
+		if column in GROUP_BY:
 			continue
-		for stat in STATISTICAL_QUANTITIES:
-			for package in ['matplotlib', 'plotly']:
-				fig = mpl.manager.new(
-					title = f'{column[:column.find("(")][:-1]} {stat}',
-					subtitle = f'Data set: {bureaucrat.measurement_name}',
-					xlabel = 'Laser DAC',
-					ylabel = column,
-					package = package,
-				)
-				for ch in sorted(set(data['n_channel'])):
-					x_vals = data.loc[data['n_channel']==ch, ['n_DAC', 'DAC value']].groupby(['n_DAC']).mean()['DAC value']
-					if stat == 'mean':
-						y_vals = data.loc[data['n_channel']==ch, ['n_DAC', column]].groupby(['n_DAC']).mean()[column]
-					elif stat == 'std':
-						y_vals = data.loc[data['n_channel']==ch, ['n_DAC', column]].groupby(['n_DAC']).std()[column]
-					fig.plot(
-						x_vals,
-						y_vals,
-						label = f'CH {ch}',
-						marker = '.',
-					)
-				mpl.manager.save_all(mkdir = bureaucrat.processed_data_dir_path/Path('png' if package=='matplotlib' else 'html'))
-		fig = mpl.manager.new(
-			title = f'{column[:column.find("(")][:-1]}',
-			subtitle = f'Data set: {bureaucrat.measurement_name}',
-			xlabel = 'Laser DAC',
-			ylabel = column,
-			package = 'plotly',
+		fig = line(
+			data_frame = averaged_df,
+			x = 'Laser DAC',
+			y = column,
+			color = 'n_channel',
+			line_dash = 'n_pulse',
+			symbol = 'n_pulse',
+			markers = True,
+			title = f'{column}<br><sup>Measurement: {bureaucrat.measurement_name}</sup>',
 		)
-		for ch in sorted(set(data['n_channel'])):
-			fig.error_band(
-				x_vals,
-				y = data.loc[data['n_channel']==ch, ['n_DAC', column]].groupby(['n_DAC']).mean()[column],
-				ylow = data.loc[data['n_channel']==ch, ['n_DAC', column]].groupby(['n_DAC']).mean()[column] - data.loc[data['n_channel']==ch, ['n_DAC', column]].groupby(['n_DAC']).std()[column],
-				ytop = data.loc[data['n_channel']==ch, ['n_DAC', column]].groupby(['n_DAC']).mean()[column] + data.loc[data['n_channel']==ch, ['n_DAC', column]].groupby(['n_DAC']).std()[column],
-										label = f'CH {ch}',
-				marker = '.',
-			)
-		mpl.manager.save_all(mkdir = bureaucrat.processed_data_dir_path/Path('error band plots'))
+		fig.write_html(str(mean_std_plots_dir_Path/Path(f'{column}.html')), include_plotlyjs='cdn')
 	
-	fig = mpl.manager.new(
-		title = f'Normalized collected charge',
-		subtitle = f'Data set: {bureaucrat.measurement_name}',
-		xlabel = 'Laser DAC',
-		ylabel = 'Normalized collected charge',
+	error_band_plots_dir_Path = bureaucrat.processed_data_dir_path/Path('error_band_plots')
+	error_band_plots_dir_Path.mkdir(parents=True, exist_ok=True)
+	for column in measured_data_df:
+		if column in GROUP_BY + ['When']:
+			continue
+		fig = line(
+			data_frame = averaged_df,
+			x = 'Laser DAC',
+			y = f'{column} mean',
+			error_y = f'{column} std',
+			error_y_mode = 'band',
+			color = 'n_channel',
+			line_dash = 'n_pulse',
+			symbol = 'n_pulse',
+			markers = True,
+			title = f'{column}<br><sup>Measurement: {bureaucrat.measurement_name}</sup>',
+		)
+		fig.write_html(str(error_band_plots_dir_Path/Path(f'{column}.html')), include_plotlyjs='cdn')
+	
+	average_waveforms_df = pandas.read_feather(bureaucrat.processed_by_script_dir_path('scan_laser_intensity.py')/Path('average_waveforms.fd'))
+	fig = line(
+		title = f'Waveforms<br><sup>Measurement {bureaucrat.measurement_name}</sup>',
+		data_frame = average_waveforms_df,
+		x = 'Time (s)',
+		y = 'Amplitude mean (V)',
+		color = 'n_pulse',
+		animation_frame = 'n_DAC',
+		facet_row = 'n_channel',
 	)
-	normalized_collected_charge_df = pandas.DataFrame({'n_DAC': n_DAC})
-	for ch in sorted(set(data['n_channel'])):
-		normalized_collected_charge_df[f'channel {ch} average'] = data.loc[data['n_channel']==ch, ['n_DAC','Collected charge (V s)']].groupby(['n_DAC']).mean()
-		normalized_collected_charge_df[f'channel {ch} std'] = data.loc[data['n_channel']==ch, ['n_DAC','Collected charge (V s)']].groupby(['n_DAC']).std()
-		normalized_collected_charge_df[f'channel {ch} average'] -= normalized_collected_charge_df[f'channel {ch} average'].min()
-		normalization_factor = normalized_collected_charge_df[f'channel {ch} average'].max()
-		normalized_collected_charge_df[f'channel {ch} average'] /= normalization_factor
-		normalized_collected_charge_df[f'channel {ch} std'] /= normalization_factor
-		fig.error_band(
-			x_vals,
-			y = normalized_collected_charge_df[f'channel {ch} average'],
-			ylow = normalized_collected_charge_df[f'channel {ch} average'] - normalized_collected_charge_df[f'channel {ch} std'],
-			ytop = normalized_collected_charge_df[f'channel {ch} average'] + normalized_collected_charge_df[f'channel {ch} std'],
-			label = f'CH {ch}',
-			marker = '.',
-		)
-	
-	
-	for ch_A in sorted(set(data['n_channel'])):
-		for ch_B in sorted(set(data['n_channel'])):
-			if ch_A == ch_B:
-				continue
-			fig = mpl.manager.new(
-				title = f'Sum of channels {ch_A} and {ch_B}',
-				subtitle = f'Data set: {bureaucrat.measurement_name}',
-				xlabel = 'Laser DAC',
-				ylabel = 'Normalized collected charge',
-			)
-			for ch in [ch_A,ch_B]:
-				fig.error_band(
-					distance,
-					y = normalized_collected_charge_df[f'channel {ch} average'],
-					ylow = normalized_collected_charge_df[f'channel {ch} average'] - normalized_collected_charge_df[f'channel {ch} std'],
-					ytop = normalized_collected_charge_df[f'channel {ch} average'] + normalized_collected_charge_df[f'channel {ch} std'],
-					label = f'CH {ch}',
-					marker = '.',
-				)
-			summed_charge_mean = normalized_collected_charge_df[f'channel {ch_A} average'] + normalized_collected_charge_df[f'channel {ch_B} average']
-			summed_charge_std = normalized_collected_charge_df[f'channel {ch_A} std'] + normalized_collected_charge_df[f'channel {ch_B} std']
-			fig.error_band(
-				distance,
-				y = summed_charge_mean,
-				ylow = summed_charge_mean - summed_charge_std,
-				ytop = summed_charge_mean + summed_charge_std,
-				label = f'CH {ch_A} + CH {ch_B}',
-				marker = '.',
-			)
-	mpl.manager.save_all(mkdir = bureaucrat.processed_data_dir_path/Path('error band plots'))
+	fig.update_yaxes(range=[average_waveforms_df['Amplitude mean (V)'].min(), average_waveforms_df['Amplitude mean (V)'].max()])
+	fig.update_layout(transition={'duration': 1})
+	fig.write_html(str(bureaucrat.processed_data_dir_path/Path('waveforms.html')), include_plotlyjs='cdn')
 	
 if __name__ == '__main__':
 	import argparse
