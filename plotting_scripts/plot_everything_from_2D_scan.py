2a41cf5f5fb8333954887abfc03ebc53364d7aac
Matias Senger <m.senger@hotmail.com>
2022-03-07 16:41:12 +0100

    Major update

--- plotting_scripts/plot_everything_from_2D_scan.py
+++ plotting_scripts/plot_everything_from_2D_scan.py
@@ -2,7 +2,7 @@ from data_processing_bureaucrat.Bureaucrat import Bureaucrat
 import numpy as np
 from pathlib import Path
 import pandas
-import grafica
+import plotly.graph_objects as go
 
 def script_core(directory):
 	bureaucrat = Bureaucrat(
@@ -11,43 +11,53 @@ def script_core(directory):
 	)
 	
 	try:
-		data_df = pandas.read_feather(bureaucrat.processed_by_script_dir_path('scan_2D.py')/Path('measured_data.fd'))
+		measured_data_df = pandas.read_feather(bureaucrat.processed_by_script_dir_path('scan_2D.py')/Path('measured_data.fd'))
 	except FileNotFoundError:
-		data_df = pandas.read_csv(bureaucrat.processed_by_script_dir_path('scan_2D.py')/Path('measured_data.csv'))
+		measured_data_df = pandas.read_csv(bureaucrat.processed_by_script_dir_path('scan_2D.py')/Path('measured_data.csv'))
 	
-	n_positions_1 = [n for n in range(max(data_df['n_position_1']))]
-	n_positions_2 = [n for n in range(max(data_df['n_position_2']))]
-	
-	for column in data_df:
-		if column in {'n_position_1', 'n_position_2', 'n_trigger', 'n_channel', 'n_pulse'}:
+	mean_df = measured_data_df.groupby(by=['n_position_1','n_position_2','n_channel','n_pulse']).mean()
+	mean_df = mean_df.reset_index()
+	for col in sorted(measured_data_df.columns):
+		if col in {'n_position','n_position_1','n_position_2','n_channel','n_pulse','n_trigger','index'}:
 			continue
-		# For each column, plot the mean and the std as scatter plots ---
-		for n_pulse in sorted(set(data_df['n_pulse'])):
-			for stat in {'mean','std'}:
-				for package in {'matplotlib', 'plotly'}:
-					for n_channel in sorted(set(data_df['n_channel'])):
-						this_channel_this_pulse_df = data_df[(data_df['n_channel']==n_channel)&(data_df['n_pulse']==n_pulse)]
-						if stat == 'mean':
-							pivottable_for_one_channel_one_pulse = pandas.pivot_table(this_channel_this_pulse_df, values=column, index='n_position_2', columns='n_position_1', aggfunc=np.nanmean)
-						elif stat == 'std':
-							pivottable_for_one_channel_one_pulse = pandas.pivot_table(this_channel_this_pulse_df, values=column, index='n_position_2', columns='n_position_1', aggfunc=np.nanstd)
-						else:
-							raise ValueError(f'Dont know what is stat {repr(stat)}.')
-						fig = grafica.new(
-							title = f'{column[:column.find("(")][:-1]} {stat} n_channel {n_channel} n_pulse {n_pulse}',
-							subtitle = f'Data set: {bureaucrat.measurement_name}',
-							xlabel = 'x (m)',
-							ylabel = 'y (m)',
-							plotter_name = package,
-							aspect = 'equal',
-						)
-						fig.heatmap(
-							x = sorted(set(data_df['x (m)'])),
-							y = sorted(set(data_df['y (m)'])),
-							z = pivottable_for_one_channel_one_pulse,
-							zlabel = column,
-						)
-					grafica.save_unsaved(mkdir = bureaucrat.processed_data_dir_path/Path('png' if package=='matplotlib' else 'html'))
+		for n_channel in set(measured_data_df['n_channel']):
+			for n_pulse in set(measured_data_df['n_pulse']):
+				df = mean_df.query(f'n_channel=={n_channel}')
+				df = df.query(f'n_pulse=={n_pulse}')
+				df = pandas.pivot_table(
+					df,
+					index = 'n_position_1',
+					columns = 'n_position_2',
+				)
+				try:
+					df[col]
+				except KeyError:
+					continue
+				fig = go.Figure()
+				figure_name = f'{col} mean value n_channel {n_channel} n_pulse {n_pulse}'
+				fig.update_layout(
+					title = f'{figure_name}<br><sup>Measurement: {bureaucrat.measurement_name}</sup>',
+					xaxis_title = 'n_position_1',
+					yaxis_title = 'n_position_2',
+				)
+				fig.add_trace(
+					go.Heatmap(
+						x = df.index.tolist(),
+						y = df[col].columns.tolist(),
+						z = df[col].values.tolist(),
+						hovertemplate = f'n<sub>1</sub>: %{{x}}, n<sub>2</sub>: %{{y}}<br>{col}: %{{z}}',
+						name = '',
+						colorbar = dict(title = col),
+					)
+				)
+				fig.update_yaxes(
+					scaleanchor = "x",
+					scaleratio = 1,
+				)
+				fig.write_html(
+					str(bureaucrat.processed_data_dir_path/Path(f'{figure_name}.html')),
+					include_plotlyjs = 'cdn',
+				)
 	
 if __name__ == '__main__':
 	import argparse
