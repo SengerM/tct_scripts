29e1e6d86666b476568c9805200c2c83470e0477
Matias Senger <m.senger@hotmail.com>
2022-03-07 18:00:42 +0100

    Some updates

--- scan_1D_sweeping_bias_voltage.py
+++ scan_1D_sweeping_bias_voltage.py
@@ -11,9 +11,14 @@ import utils
 import tct_scripts_config
 
 OSCILLOSCOPE_CHANNELS = [1,2]
-LASER_DAC = 573
+LASER_DAC = int(2000/3300*2**10)
 N_TRIGGERS_PER_POSITION = 55
-BIAS_VOLTAGES = np.linspace(55,500,22)
+BIAS_VOLTAGES = [int(V) for V in utils.interlace(np.linspace(111,500,4))] + list(np.linspace(522,650,6).astype(int))
+
+print(f'Bias voltages = {BIAS_VOLTAGES}')
+if input(f'Continue? (YeS) : ') != 'YeS':
+	raise ValueError(f'The input was not `YeS`.')
+
 STEP_SIZE = 1e-6
 SWEEP_LENGTH = 333e-6
 
@@ -25,14 +30,10 @@ center = tuple([center[k] for k in sorted(center.keys())]) # Sorted x y z
 X_MIDDLE = center[0]
 Y_MIDDLE = center[1]
 Z_FOCUS = center[2]
-x_positions = X_MIDDLE + np.linspace(-SWEEP_LENGTH/2,SWEEP_LENGTH/2,int(SWEEP_LENGTH/STEP_SIZE))*np.sin(np.pi/4)
-y_positions = Y_MIDDLE - np.linspace(-SWEEP_LENGTH/2,SWEEP_LENGTH/2,int(SWEEP_LENGTH/STEP_SIZE))*np.sin(np.pi/4)
-z_positions = Z_FOCUS + x_positions*0
-positions = list(zip(x_positions,y_positions,z_positions))
-
-BIAS_VOLTAGES = [int(V) for V in utils.interlace(BIAS_VOLTAGES)]
 
 the_setup = TheSetup()
+
+the_setup.current_compliance = 100e-6
 
 device_name = input('Device name? ').replace(' ','_')
 
@@ -43,10 +44,23 @@ bureaucrat = Bureaucrat(
 )
 time.sleep(1)
 
+if 'preview' in bureaucrat.measurement_name.lower():
+	print(f'PREVIEW MODE!!!!')
+	STEP_SIZE = 11e-6
+	BIAS_VOLTAGES = [111]
+	N_TRIGGERS_PER_POSITION = 22
+
 reporter = TelegramReporter(
 	telegram_token = TelegramReportingInformation().token, 
 	telegram_chat_id = TelegramReportingInformation().chat_id,
 )
+
+################################
+ANGLE = np.pi/4
+x_positions = X_MIDDLE + np.linspace(-SWEEP_LENGTH/2,SWEEP_LENGTH/2,int(SWEEP_LENGTH/STEP_SIZE))*np.sin(ANGLE)
+y_positions = Y_MIDDLE - np.linspace(-SWEEP_LENGTH/2,SWEEP_LENGTH/2,int(SWEEP_LENGTH/STEP_SIZE))*np.sin(ANGLE)
+z_positions = Z_FOCUS + x_positions*0
+positions = list(zip(x_positions,y_positions,z_positions))
 
 with reporter.report_for_loop(len(x_positions)*N_TRIGGERS_PER_POSITION*len(BIAS_VOLTAGES), f'{bureaucrat.measurement_name}') as reporter:
 	with open(bureaucrat.processed_data_dir_path/Path(f'README.txt'),'w') as ofile:
