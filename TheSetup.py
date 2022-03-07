8280d3a26aa53e3f25c27aa85cd2ecd5f9d34179
Matias Senger <m.senger@hotmail.com>
2022-03-07 17:57:33 +0100

    Changed LeCroy oscilloscope by PSI DRS4 Evaluation Board V5

--- TheSetup.py
+++ TheSetup.py
@@ -1,12 +1,12 @@
 import PyticularsTCT # https://github.com/SengerM/PyticularsTCT
 import pyvisa
-import TeledyneLeCroyPy # https://github.com/SengerM/TeledyneLeCroyPy
 from keithley.Keithley2470 import Keithley2470SafeForLGADs # https://github.com/SengerM/keithley
 from Pyro5.api import Proxy
 import atexit
 from time import sleep
 import threading
 import tct_scripts_config
+from pydrs import PythonFriendlyBoard # https://github.com/SengerM/pydrs
 
 class TheSetup:
 	"""This class wraps all the hardware so if there are changes it is easy to adapt."""
@@ -14,14 +14,15 @@ class TheSetup:
 		"""
 		- safe_mode: Turns laser and high voltage off when your Python instance is finished using `atexit`. Temperature is not touched.
 		"""
-		self._oscilloscope = TeledyneLeCroyPy.LeCroyWaveRunner(pyvisa.ResourceManager().open_resource('USB0::1535::4131::2810N60091::0::INSTR'))
+		# ~ self._LeCroy = TeledyneLeCroyPy.LeCroyWaveRunner(pyvisa.ResourceManager().open_resource('USB0::1535::4131::2810N60091::0::INSTR'))
+		self._drs4_evaluation_board = PythonFriendlyBoard()
 		self._tct = PyticularsTCT.TCT(
 			x_stage_port = '/dev/ttyACM3',
 			y_stage_port = '/dev/ttyACM2',
 			z_stage_port = '/dev/ttyACM1',
 		)
 		self._keithley = Keithley2470SafeForLGADs('USB0::1510::9328::04481179::0::INSTR', polarity = 'negative')
-		self._temperature_controller = Proxy(tct_scripts_config.TEMPERATURE_CONTROLLER_URI)
+		# ~ self._temperature_controller = Proxy(tct_scripts_config.TEMPERATURE_CONTROLLER_URI)
 		
 		# Threading locks ---
 		self._oscilloscope_Lock = threading.RLock()
@@ -123,46 +124,63 @@ class TheSetup:
 	def configure_oscilloscope_for_two_pulses(self):
 		"""Configures the horizontal scale and trigger of the oscilloscope to properly acquire two pulses."""
 		with self._oscilloscope_Lock:
-			self._oscilloscope.set_trig_source('ext')
-			self._oscilloscope.set_trig_level('ext', -175e-3) # Totally empiric.
-			self._oscilloscope.set_trig_coupling('ext', 'DC')
-			self._oscilloscope.set_trig_slope('ext', 'negative')
-			self._oscilloscope.set_tdiv('20ns')
-			self._oscilloscope.set_trig_delay(-43e-9) # Totally empiric.
+			# ~ self._LeCroy.set_trig_source('ext')
+			# ~ self._LeCroy.set_trig_level('ext', -175e-3) # Totally empiric.
+			# ~ self._LeCroy.set_trig_coupling('ext', 'DC')
+			# ~ self._LeCroy.set_trig_slope('ext', 'negative')
+			# ~ self._LeCroy.set_tdiv('20ns')
+			# ~ self._LeCroy.set_trig_delay(-43e-9) # Totally empiric.
+			self._drs4_evaluation_board.set_sampling_frequency(Hz=3.5e9)
+			self._drs4_evaluation_board.set_transparent_mode('on')
+			self._drs4_evaluation_board.set_input_range(center=0)
+			self._drs4_evaluation_board.enable_trigger(True,False) # Don't know what this line does, it was in the example `drs_exam.cpp`.
+			self._drs4_evaluation_board.set_trigger_source('ch4')
+			self._drs4_evaluation_board.set_trigger_level(volts=-.1)
+			self._drs4_evaluation_board.set_trigger_polarity(edge='falling')
+			self._drs4_evaluation_board.set_trigger_delay(seconds=100e-9)
 	
 	def wait_for_trigger(self):
 		"""Blocks execution until there is a trigger in the oscilloscope."""
 		with self._oscilloscope_Lock:
-			self._oscilloscope.wait_for_single_trigger()
+			# ~ self._LeCroy.wait_for_single_trigger()
+			self._drs4_evaluation_board.wait_for_single_trigger()
 	
 	def get_waveform(self, channel: int):
 		"""Gets the waveform from the oscilloscope for the respective channel."""
 		with self._oscilloscope_Lock:
-			return self._oscilloscope.get_waveform(channel=channel)
+			# ~ return self._LeCroy.get_waveform(channel=channel)
+			waveform_data = self._drs4_evaluation_board.get_waveform(channel)
+			waveform_data['Amplitude (V)'] *= -1
+			return waveform_data
 	
 	def set_oscilloscope_vdiv(self, channel: int, vdiv: float):
 		"""Sets the osciloscope's Volts per division."""
-		with self._oscilloscope_Lock:
-			self._oscilloscope.set_vdiv(channel, vdiv)
+		pass
+		# ~ with self._oscilloscope_Lock:
+			# ~ self._LeCroy.set_vdiv(channel, vdiv)
 	
 	# Temperature and humidity sensor ----------------------------------
 	
 	@property
 	def temperature(self):
 		"""Returns a reading of the temperature as a float number in Celsius."""
-		return self._temperature_controller.temperature
+		try:
+			return self._temperature_controller.temperature
+		except AttributeError: # If there is no temperature sensor defined...
+			return float('NaN')
 	
 	@property
 	def humidity(self):
 		"""Returns a reading of the humidity as a float number in %RH."""
-		return self._temperature_controller.humidity
+		try:
+			return self._temperature_controller.humidity
+		except AttributeError: # If there is no humidity sensor defined...
+			return float('NaN')
 	
 if __name__ == '__main__':
 	import time
 	
-	the_setup = TheSetup(
-		temperature_controller_Pyro_uri = 'PYRO:temperature_controller@0.0.0.0:41995',
-	)
+	the_setup = TheSetup()
 	
 	print(f'Temperature = {the_setup.temperature:.2f} °C, humidity = {the_setup.humidity:.2f} %RH')
 
