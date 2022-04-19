9be52f0925bc43b6ff85a9db98063d92af738923
Matias Senger <m.senger@hotmail.com>
2022-04-19 15:54:54 +0200

    Updated various things

--- z_scan_to_find_focus.py
+++ z_scan_to_find_focus.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scan_1D import script_core as linear_scan
+from scan_1D import script_core as linear_scan, post_process
 from TheSetup import TheSetup
 import pandas
 from pathlib import Path
@@ -9,7 +9,7 @@ from grafica.plotly_utils.utils import line as grafica_line
 
 STEP_SIZE = 22e-6
 SWEEP_LENGTH = 8e-3/5
-Z_MIDDLE = 71.42597656250001e-3
+Z_MIDDLE = 71.41470703125e-3
 
 the_setup = TheSetup()
 
@@ -22,20 +22,21 @@ y_positions = z_positions*0 + current_position[1]
 measurement_base_path = linear_scan(
 	measurement_name = input('Measurement name? ').replace(' ', '_'),
 	the_setup = the_setup,
-	bias_voltage = 99,
-	laser_DAC = 610,
+	bias_voltage = 111,
+	laser_DAC = 633,
 	positions = list(zip(x_positions,y_positions,z_positions)),
-	n_triggers = 44,
-	acquire_channels = [1,2],
+	n_triggers = 2,
+	acquire_channels = [2,3],
 )
+post_process(measurement_base_path, silent=False)
 
 bureaucrat = Bureaucrat(
-	str(Path(measurement_base_path)),
+	Path(measurement_base_path),
 	variables = locals(),
 	new_measurement = False,
 )
 
-data_df = pandas.read_feather(bureaucrat.processed_by_script_dir_path('scan_1D.py')/Path('measured_data.fd'))
+data_df = pandas.read_feather(bureaucrat.processed_by_script_dir_path('parse_waveforms_from_scan_1D.py')/Path('data.fd'))
 
 mean_df = data_df.groupby(by=['n_position','n_channel','n_pulse']).agg(['mean','std'])
 mean_df.columns = [' '.join(col).strip() for col in mean_df.columns.values]
@@ -49,5 +50,9 @@ fig = grafica_line(
 	error_y_mode = 'band',
 	symbol = 'n_pulse',
 	color = 'n_channel',
+	labels = {
+		'z (m) mean': 'z (m)',
+		'Collected charge (V s) mean': 'Collected charge (V s)'
+	},
 )
 fig.write_html(str(bureaucrat.processed_data_dir_path/Path('collected_charge_vs_z.html')), include_plotlyjs='cdn')
